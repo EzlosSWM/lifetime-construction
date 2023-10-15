bd08d9154e51ecd186c918f057cd7aa1a9207d71
EzlosSWM <ezlosswm@gmail.com>
2023-10-14 21:45:18 -0600

    testing: bg img

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
                 bottom: '0px 4px 0px -1px rgba(0, 0, 0, 0.2), 0px 7px 4px -9px rgba(0, 0, 0, 0.1)'
             },
 			backgroundImage: {
-				'hero': "url(./hero-img.jpg)",
+				'hero': "url('../hero-img.jpg')",
                 'cta': "url('./images/concrete-bg.jpg')"
 			}
         },
