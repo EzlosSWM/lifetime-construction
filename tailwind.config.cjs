1f78f478d974502d06a9a5577e5bb93c0f740426
EzlosSWM <ezlosswm@gmail.com>
2023-10-14 22:33:38 -0600

    fixed: button component & imgs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
             },
 			backgroundImage: {
 				'hero': "url('../hero-img.jpg')",
-                'cta': "url('./images/concrete-bg.jpg')"
+                'cta': "url('../images/concrete-bg.jpg')"
 			}
         },
 	},
